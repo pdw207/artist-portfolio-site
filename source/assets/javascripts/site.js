df5486fe6a6bbba0c0d2ff034df62276bf20b17e
Paul Wilson <paul@vermonster.com>
2015-06-15 22:04:47 -0400

    Update images

--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -1,6 +1,7 @@
 window.onload = function() {
 
   $(document).foundation();
+
   // Create Isotope Container for Artwork
   $('#portfolio').isotope({
     itemSelector: '.art',
@@ -110,24 +111,4 @@ $(document).foundation({
     bullets: false
   }
 });
-
-
-////////////////////////////////////////
-//
-// Avoid `console` errors in browsers that lack a console.
-//
-////////////////////////////////////////
-
-
-if (!(window.console && console.log)) {
-    (function() {
-        var noop = function() {};
-        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
-        var length = methods.length;
-        var console = window.console = {};
-        while (length--) {
-            console[methods[length]] = noop;
-        }
-    }());
-}
 
